d61cad7dea42cea9c77e1bbcdb9b8f989c5f7cbb
roman-palenha <roman.palenha04@gmail.com>
2022-07-25 19:06:26 +0300

    created simple func

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+function add() {
+    if(document.getElementById('getText').value == '') {
+        alert('Empty field!');
+        return;
+    }
+    let todoList = document.getElementById('list');
+    let item = document.createElement('div')
+    item.className = 'todo-list-item';
+    let itemText = document.createElement('h1');
+    itemText.textContent = document.getElementById('getText').value;
+    let removeBtn = document.createElement('button');
+    removeBtn.onclick = function() { this.parentNode.remove(); };
+    removeBtn.textContent = 'Remove';
+    item.appendChild(itemText);
+    item.appendChild(removeBtn);
+    todoList.appendChild(item);
+}
+
