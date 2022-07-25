be70533c262f26f47b5eb8b367fcc31105efd048
roman-palenha <roman.palenha04@gmail.com>
2022-07-25 19:35:31 +0300

    added edit btn

--- scripts/script.js
+++ scripts/script.js
@@ -7,11 +7,25 @@ function add() {
     let item = document.createElement('div')
     item.className = 'todo-list-item';
     let itemText = document.createElement('h1');
+    itemText.id = 'todotext';
     itemText.textContent = document.getElementById('getText').value;
+    let editBtn = document.createElement('button');
+    editBtn.onclick = function() { 
+        let todoText = this.parentNode.childNodes[0].textContent;
+        var editedText = prompt("Enter new todo: ");
+        if(editedText !== '')
+            this.parentNode.childNodes[0].textContent = editedText;
+        else 
+        alert("Enter not empty value");
+    };
+    editBtn.textContent = 'Edit';
     let removeBtn = document.createElement('button');
-    removeBtn.onclick = function() { this.parentNode.remove(); };
+    removeBtn.onclick = function() {
+        this.parentNode.remove();
+     };
     removeBtn.innerHTML = '<img src="imgs/remove.png" />'
     item.appendChild(itemText);
+    item.appendChild(editBtn);
     item.appendChild(removeBtn);
     todoList.appendChild(item);
 }
