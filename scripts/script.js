d9d045fd4a56e0341440a6ff88c11e069e1c132d
roman-palenha <roman.palenha04@gmail.com>
2022-07-29 01:10:18 +0300

    added some adjustments

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,77 @@
 function add() {
+    let itemToSave = addDiv(document.getElementById('getText').value, false, new Date().toJSON());
+    if(itemToSave != undefined)
+        localStorage.setItem(JSON.parse(itemToSave).id, itemToSave);
+}
+
+function markTask(textNode) {
+    if(textNode.style.textDecoration == 'line-through') {
+        textNode.style.textDecoration = 'none';   
+        return false;
+     } else {
+        textNode.style.textDecoration = 'line-through';
+        return true;
+     }
+} 
+
+function showTasks() {
+    let todoList = document.getElementById('list'); 
+    for(let key in localStorage) {
+           if (!localStorage.hasOwnProperty(key) || localStorage.getItem(key) == undefined) {
+                continue;
+            }
+        let item = JSON.parse(localStorage.getItem(key));
+        addDiv(item.text, item.completed, item.id);
+    }
+}
+
+function addDiv(todoText, isCompleted, date) {
     if(document.getElementById('getText').value == '') {
         alert('Empty field!');
         return;
     }
-    let todoList = document.getElementById('list');
+    let todoList = document.getElementById('list'); 
     let item = document.createElement('div')
     item.className = 'todo-list-item';
+    item.id = date;
     let itemText = document.createElement('h1');
-    itemText.id = 'todotext';
-    itemText.textContent = document.getElementById('getText').value;
+    itemText.textContent = todoText;
+    itemText.addEventListener('click', () => { 
+      localStorage.setItem(item.id, JSON.stringify({ id: item.id, text: todoText, 
+        completed: markTask(itemText) }));
+     });
+    if(Date.now() - new Date(date) > 3 * 24 * 1000 * 3600)
+        itemText.style.color = '#8d0000';
+    else if(Date.now() - new Date(date) > 24 * 1000 * 3600)
+        itemText.style.color = '#eb1111';
     let editBtn = document.createElement('button');
     editBtn.onclick = function() { 
-        let todoText = this.parentNode.childNodes[0].textContent;
         var editedText = prompt("Enter new todo: ");
-        if(editedText !== '')
+        if(editedText != '' || editedText != null) {
             this.parentNode.childNodes[0].textContent = editedText;
-        else 
-        alert("Enter not empty value");
+            let i = JSON.parse(localStorage.getItem(this.parentNode.id));
+            i.text = editedText;
+            localStorage.setItem(item.id, JSON.stringify({ id: item.id, text: editedText, 
+                completed: i.completed }));
+        } else {
+            alert("Enter not empty value");
+        }
     };
+    editBtn.id = 'editBtn';
     editBtn.textContent = 'Edit';
     let removeBtn = document.createElement('button');
     removeBtn.onclick = function() {
         this.parentNode.remove();
+        localStorage.removeItem(this.parentNode.id);
      };
-    removeBtn.innerHTML = '<img src="imgs/remove.png" />'
+    removeBtn.textContent = 'Х';
+    if(isCompleted) 
+        itemText.style.textDecoration = 'line-through';
     item.appendChild(itemText);
     item.appendChild(editBtn);
     item.appendChild(removeBtn);
     todoList.appendChild(item);
-}
 
+    return JSON.stringify({ id: item.id, text: item.childNodes[0].textContent, 
+        completed: item.childNodes[0].style.textDecoration == 'line-through' });
+}
